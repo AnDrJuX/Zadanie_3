910a2a9b333d269000e3fcd3faa02a5fae2a3f89
Andres Peterson <andres_p@mail.ru>
2017-05-23 20:22:51 +0300

    add spec GET #new.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
   let(:answer) { create(:answer) }
 
+  describe 'GET #new' do
+    before do
+      get :new, params: { question_id: question.id }
+    end
+
+    it 'assigns Question from DB to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'assigns new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+
 end
