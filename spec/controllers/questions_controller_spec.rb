8842f0b826d4f98f19a75a80652ac22051cf8e02
Andres Peterson <andres_p@mail.ru>
2017-05-23 05:56:21 +0300

    add spec to questions_controller_spec.rb

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  it 'populates an array of all questions' do
+    question1 = FactoryGirl.create(:question)
+    question2 = FactoryGirl.create(:question)
+
+    get :index
+
+    expect(assigns(:questions)).to match_array([question1, question2])
+  end
+
+  it 'renders index wiew' do
+    get :index
+    expect(response).to render_template :index
+  end
 
 end
