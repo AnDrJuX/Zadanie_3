829d303b5f4eb9c803eebb1c0580dd07fb173cfc
Andres Peterson <andres_p@mail.ru>
2017-05-23 18:16:22 +0300

    add all question specs.

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -41,4 +41,26 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates and saves new question to DB' do
+        expect { post :create, params: { question: attributes_for(:question)} }.to change(Question, :count).by(1)
+      end
+      it 'redirects to question view' do
+        post :create, params: { question: attributes_for(:question)}
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'fails to save new question to DB' do
+        expect { post :create, params: { question: attributes_for(:invalid_question)} }.to_not change(Question, :count)
+      end
+      it 'renders new view' do
+        post :create, params: { question: attributes_for(:invalid_question)}
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
